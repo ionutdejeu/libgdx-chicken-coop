78007924e8e623f64d1a3b9e0535dc3370e3ef76
Ionut <ionut.dejeu@gamail.com>
2021-10-10 13:20:34 +0300

    Initial project commit

---
+++ desktop/src/com/chickencoop/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.chickencoop.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.chickencoop.game.ChickenCoop;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ChickenCoop(), config);
+	}
+}
